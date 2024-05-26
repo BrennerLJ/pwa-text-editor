bd733aeab787e17e9e04a7265f0a622b411b64a2
Brenner Jolley <bjbrenardjoey@gmail.com>
2024-05-25 23:16:22 -0600

    added asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,23 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) =>
+  request.destination === 'style' || request.destination === 'script' || request.destination === 'image',
+
+  new CacheFirst({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
+registerRoute(({ request }) => request.destination === 'style', assetCache);
+registerRoute(({ request }) => request.destination === 'script', assetCache);
+registerRoute(({ request }) => request.destination === 'image', assetCache);
